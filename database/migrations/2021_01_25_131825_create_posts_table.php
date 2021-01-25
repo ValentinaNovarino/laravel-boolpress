f8238c8d705ee4db8c17295d7280503925435952
ValentinaNovarino <valenovarino@gmail.com>
2021-01-25 14:44:42 +0100

    creato model + migration + table

---
+++ database/migrations/2021_01_25_131825_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('author');
+            $table->string('title', 40);
+            $table->string('subtitle');
+            $table->string('images');
+            $table->longtext('content');
+            $table->string('category', 20);
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
