6a2ea79a3a82931ab6787b6b0f87beaaeb7b9e0a
ValentinaNovarino <valenovarino@gmail.com>
2021-01-25 17:39:44 +0100

    corretto formattazione colonne

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -16,10 +16,10 @@ class PostsTableSeeder extends Seeder
         for ($i=0; $i < 99; $i++) {
             $new_post = new Post();
             $new_post->author = $faker->name();
-            $new_post->title = $faker->word();
+            $new_post->title = $faker->words(3,true);
             $new_post->subtitle = $faker->sentence();
             $new_post->images = $faker->imageUrl(360, 360, 'animals', true);
-            $new_post->content = $faker->text();
+            $new_post->content = $faker->text(500);
             $new_post->category = $faker->word();
             $new_post->date = $faker->date();
             $new_post->save();
