88781e23dd09cec3d29bb51bfd49d816acbd19ae
ValentinaNovarino <valenovarino@gmail.com>
2021-01-25 15:20:44 +0100

    creato seeder e popolato tabella

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker;
 
 class PostsTableSeeder extends Seeder
 {
@@ -9,8 +11,18 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 99; $i++) {
+            $new_post = new Post();
+            $new_post->author = $faker->name();
+            $new_post->title = $faker->word();
+            $new_post->subtitle = $faker->sentence();
+            $new_post->images = $faker->imageUrl(360, 360, 'animals', true);
+            $new_post->content = $faker->text();
+            $new_post->category = $faker->word();
+            $new_post->date = $faker->date();
+            $new_post->save();
+        };
     }
 }
